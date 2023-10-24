72720e508643e84135488734c930215334bf4cfa
fleetingclarity <fleetingclarity@proton.me>
2023-10-23 20:27:47 -0500

    still needs every commit from deploys in lead time

--- data-generator/tools/init-db.sql
+++ data-generator/tools/init-db.sql
@@ -8,7 +8,7 @@ CREATE TABLE events_raw (
     event_type VARCHAR(50) NOT NULL,
     metadata JSONB,
     time_created TIMESTAMP NOT NULL,
-    signature VARCHAR(255),
+    signature VARCHAR(100),
     msg_id BIGINT,
     source VARCHAR(50)
 );
@@ -16,9 +16,18 @@ CREATE TABLE events_raw (
 CREATE INDEX idx_er_signature ON events_raw(signature);
 
 CREATE TABLE events_enriched (
-    events_raw_signature VARCHAR(255) NOT NULL PRIMARY KEY,
+    events_raw_signature VARCHAR(100) NOT NULL PRIMARY KEY,
     enriched_metadata JSONB
 );
+
+
+-- When a successful deployment occurs, insert a record for each commit associated to that deployment
+-- Theoretically a commit_id should probably only occur once here
+CREATE TABLE commit_deploy (
+    commit_id VARCHAR(100) UNIQUE NOT NULL,
+    deploy_id VARCHAR(100) NOT NULL
+);
+CREATE INDEX idx_cd_deploy_id on commit_deploy(deploy_id);
 
 -- Views that will be used by the front end for querying
 CREATE OR REPLACE VIEW changes AS
@@ -32,20 +41,6 @@ LATERAL jsonb_array_elements(e.metadata::jsonb->'commits') AS commit
 WHERE event_type = 'push'
 GROUP BY 1, 2, 3, 4;
 -- end view changes
-
-CREATE OR REPLACE VIEW events AS
-SELECT
-    raw.id,
-    raw.event_type,
-    raw.time_created,
-    raw.metadata,
-    enr.enriched_metadata,
-    raw.signature,
-    raw.msg_id,
-    raw.source
-FROM events_raw raw
-JOIN events_enriched enr ON raw.signature = enr.events_raw_signature;
--- end view events
 
 CREATE OR REPLACE VIEW deploys AS
    with deploys_cloudbuild_github_gitlab as (
@@ -61,14 +56,7 @@ CREATE OR REPLACE VIEW deploys AS
                 substring(metadata->>'commit_url' from '.*commit\/(.*)')
             )
             when source = 'argocd' then metadata#>>'{commit_sha}'
-        end as main_commit,
-        case
-            when source like 'github%' then array(
-                select value->>'id'
-                from jsonb_array_elements(metadata::jsonb->'commits') as value
-            )
-            else array[]::text[]
-        end as additional_commits
+        end as main_commit
     from events_raw
     where (
         (source = 'cloud_build' and metadata#>>'{status}' = 'success')
@@ -83,8 +71,7 @@ deploys_tekton as (
         source,
         id as deploy_id,
         time_created,
-        param->>'value' as main_commit,
-        array[]::text[] as additional_commits
+        param->>'value' as main_commit
     from events_raw,
     lateral jsonb_array_elements(metadata::jsonb->'data'->'pipelinerun'->'spec'->'params') as param
     where event_type = 'dev.tekton.event.pipelinerun.successful.v1'
@@ -96,8 +83,7 @@ deploys_circleci as (
         source,
         id as deploy_id,
         time_created,
-        metadata#>>'{pipeline, vcs, revision}' as main_commit,
-        array[]::text[] as additional_commits
+        metadata#>>'{pipeline, vcs, revision}' as main_commit
     from events_raw
     where source = 'circleci' and event_type = 'workflow-completed' and metadata#>>'{workflow, name}' like '%deploy%' and metadata#>>'{workflow, status}' = 'success'
 )
@@ -109,38 +95,31 @@ select * from deploys_cloudbuild_github_gitlab
 order by time_created desc;
 -- end view deploys
 
-CREATE OR REPLACE VIEW deployments AS
-with changes_raw as (
-    select
-        id,
-        metadata as change_metadata
-    from events_raw
-),
-deployment_changes AS (
+CREATE OR REPLACE VIEW deployment_changes AS (
     SELECT
-        source,
+        e.source,
         deploy_id,
-        deploys.time_created,
-        change_metadata,
-        ARRAY(
-            SELECT jsonb_array_elements_text(change_metadata#>'{commits, id}')
-        ) AS array_commits,
+        d.time_created,
+        e.metadata,
+        ARRAY_AGG(commit_data->>'id') AS array_commits,
         main_commit
-    FROM deploys
-    JOIN changes_raw ON (
-        changes_raw.id = deploys.main_commit
-        OR changes_raw.id = ANY(deploys.additional_commits)
-    )
-)
+    FROM deploys d
+    JOIN events_raw e ON e.id = d.main_commit
+    LEFT JOIN LATERAL jsonb_array_elements(e.metadata->'commits') AS commit_data ON TRUE
+    GROUP BY e.source, deploy_id, d.time_created, e.metadata, main_commit
+);
+-- end view deployment_changes
+CREATE OR REPLACE VIEW deployments AS
 SELECT
     source,
     deploy_id,
     time_created,
     main_commit,
-    ARRAY_AGG(DISTINCT array_commits) filter(where array_commits <> '{}') AS changes
-FROM deployment_changes
+    ARRAY_AGG(DISTINCT unnested_commits) AS changes
+FROM
+    deployment_changes,
+    UNNEST(array_commits) AS unnested_commits
 GROUP BY 1,2,3,4;
--- end deployments view
 
 CREATE OR REPLACE VIEW incidents AS
 SELECT
